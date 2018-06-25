780c93955936f4ac7def1dab446c02aece9c693a
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2018-06-24 23:39:56 -0400

    login endpoint for student

--- src/handlers/UserHandler.js
+++ src/handlers/UserHandler.js
@@ -16,7 +16,7 @@ class UserHandler{
   static create(request, response){
     let reply
 
-    console.log('request body', request.body)
+    //console.log('request body', request.body)
 
     const isValidJSON = Joi.validate(request.body, userSchema)
     if(isValidJSON.error !== null){
@@ -33,21 +33,52 @@ class UserHandler{
 
     bcrypt.hash(user.password, SALT_ROUNDS, (err, hashedPassword) => {
 
-      reply = {
-        data: {
-          email: user.email,
-          password: hashedPassword
-        }
-      }
+      user.password = hashedPassword
 
-      return response.status(201).send(reply)
+      const insertIntoAlumno = 'INSERT INTO ALUMNO(rut_alumno, nombre_alumno, apellido_alumno, correo_alumno, hashed_password) VALUES ($1,$2,$3,$4,$5) RETURNING *'
+
+      const alumnosValues = [
+        user.rut_alumno,
+        user.nombre_alumno,
+        user.apellido_alumno,
+        user.correo_alumno,
+        user.password
+      ]
+
+      client.query(insertIntoAlumno, alumnosValues, (err, result) => {
+
+        if(err) {
+          reply = {
+            error: {
+              message: 'Internal server error',
+              info: err
+            }
+          }
+
+          return response.status(500).send(response.json(reply))
+        }
+
+        const alumno = result.rows[0]
+
+        reply = {
+          data: {
+            alumno: {
+              rut: alumno.rut_alumno,
+              nombre: alumno.nombre_alumno,
+              apellido: alumno.apellido_alumno,
+              correo: alumno.correo_alumno,
+              hashed_password: alumno.hashed_password
+            }
+          }
+        }
+
+        return response.status(201).send(reply)
+      })
     })
   }
 
   static login(request, response){
     let reply
-
-    console.log(request.body)
 
     const isValidJSON = Joi.validate(request.body, credentialsSchema)
     if(isValidJSON.error != null){
@@ -59,14 +90,63 @@ class UserHandler{
       return response.status(422).send(reply)
     }
 
-    reply = {
-      credentials:{
-        email: request.body.credentials.email,
-        password: request.body.credentials.password
-      }
-    }
+    const { credentials : { rut, password } } = request.body
+    const selectAlumnos = 'SELECT * FROM ALUMNO WHERE rut_alumno = $1'
 
-    return response.status(200).send(reply)
+    const alumnoValues = [ rut ]
+
+    client.query(selectAlumnos, alumnoValues, (err, result) => {
+      if(err){
+        reply = {
+          error:{
+            message: 'Internal server error',
+            info: err
+          }
+        }
+        return response.status(500).send(reply)
+      }
+
+      if(result.rowCount === 0){
+        reply = {
+          error: {
+            message: 'Unauthorized. No credentials found'
+          }
+        }
+        return response.status(401).send(reply)
+      }
+
+      const alumno = result.rows[0]
+
+      bcrypt.compare(password, alumno.hashed_password, (err, isValid) => {
+        if(!isValid){
+          reply = {
+            error: {
+              message: 'Unproccessable entity'
+            }
+          }
+          return response.status(422).send(reply)
+        }
+
+        const jwtSchema = {
+          exp: moment().add(8, 'hours').unix(),
+          id: alumno.rut_alumno,
+          role: 'STUDENT'
+        }
+
+        const token = jwt.sign(jwtSchema, process.env.SECRET_KEY)
+
+        reply = {
+          data: {
+            token,
+            email: alumno.correo_alumno,
+            id: alumno.rut_alumno,
+            role: 'STUDENT'
+          }
+        }
+
+        return response.status(201).send(reply)
+      })
+    })
   }
 }
 
--- src/validators/index.js
+++ src/validators/index.js
@@ -5,16 +5,17 @@ const LIMIT_AMOUMT = 99999999
 
 export const userSchema = Joi.object().keys({
   user: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(7).required(),
-    role: Joi.any().valid(['STUDENT', 'GUARDIAN', 'EXECUTIVE']).required()
+    rut_alumno: Joi.string().required(),
+    nombre_alumno: Joi.string().required(),
+    apellido_alumno: Joi.string().required(),
+    correo_alumno: Joi.string().required(),
+    password: Joi.string().min(7).required()
   }).required()
 })
 
 export const credentialsSchema = Joi.object().keys({
   credentials: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(7).required(),
-    ROLE: Joi.any().valid(['STUDENT', 'GUARDIAN', 'EXECUTIVE']).required()
+    rut: Joi.string().required(),
+    password: Joi.string().min(7).required()
   }).required()
 })
